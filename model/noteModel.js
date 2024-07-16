f16c6fe6a2947c6a29c99ac225d5a290fdd9eef8
ankitjhagithub21 <ajha45075@gmail.com>
2024-07-16 20:27:24 +0530

    created note model

--- model/noteModel.js
+++ model/noteModel.js
@@ -1,2 +1,25 @@
 const mongoose = require('mongoose')
 
+const noteSchema = mongoose.Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    desc:{
+        type:String,
+        required:true
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now()
+    },
+    author:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    }
+
+})
+
+const Note= mongoose.model('Note',noteSchema)
+
+module.exports = Note
